daf336bc1b669b305961dd708466aeb3072c016a
breno <brenorezende.4457@aluno.una.br>
2021-10-05 19:29:18 -0300

    ex_01

---
+++ flutter_ex_1/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Text(
+      'Breno Silva Rezende',
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: Colors.red),
+    ), //Text.
+  ); //runApp.
+}
